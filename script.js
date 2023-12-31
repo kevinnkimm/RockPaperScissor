9bd4543e63a66e234e4cbbec5a3273879700f09d
kevinnkimm <43616290+kevinnkimm@users.noreply.github.com>
2023-12-31 00:18:05 -0500

    implementing prompt method for playerSelection

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        const playerSelection = "rock";
+        let playerSelection = prompt("Choose Rock, Paper, or Scissors: ");
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
     }

9efc899ab77194b8cb32504124d4eb5b3aa56cf6
kevinnkimm <43616290+kevinnkimm@users.noreply.github.com>
2024-01-02 00:19:12 -0500

    implemented ui features to the game

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// GAME
 const array = ["Rock", "Paper", "Scissors"];
 let myScore = 0;
 let cpScore = 0;
@@ -49,26 +50,67 @@ function playRound(playerSelection, computerSelection) {
 }
 
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Choose Rock, Paper, or Scissors: ");
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-        console.log(result);
+function game(playerSelection) {
+    // for (let i = 0; i < 5; i++) {
+        
+    // }
+    // let playerSelection = prompt("Choose Rock, Paper, or Scissors: ");
+    if (cpScore === 5 || myScore === 5) {
+        document.querySelectorAll('button').forEach(button => {
+            button.removeEventListeners('click', gameLogicHandler);
+        });
+        
+        const finalResult = document.createElement('div');
+        finalResult.textContent = 'You have reached maximum wins/loses';
+        document.body.appendChild(finalResult);
+        return;
     }
-    
-    if (myScore < cpScore) {
-        console.log(`Computer wins with a ${cpScore} points over ${myScore} points`);
-    }
-    else if (myScore > cpScore) {
-        console.log(`Player wins with a ${myScore} points over ${cpScore} points`);
 
-    }
-    else {
-        console.log(`draw with a ${myScore} points over ${cpScore} points`);
-    }
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
     
+    const resultDisplay = document.getElementById('result');
+    resultDisplay.textContent = result;
+
+    const scoreDisplay = document.getElementById('score');
+    scoreDisplay.textContent = `Player: ${myScore} - Computer: ${cpScore}`;
+
     
 }
 
-game();
+// UI
+const rockBtn = document.createElement('button');
+const paperBtn = document.createElement('button');
+const scissorsBtn = document.createElement('button');
+
+rockBtn.textContent = 'Rock';
+paperBtn.textContent = 'Paper';
+scissorsBtn.textContent = 'Scissors';
+
+function gameLogicHandler(event) {
+    const playerSelection = event.target.textContent;
+    game(playerSelection);
+}
+
+
+rockBtn.addEventListener('click', gameLogicHandler);
+
+paperBtn.addEventListener('click', gameLogicHandler);
+
+scissorsBtn.addEventListener('click', gameLogicHandler);
+
+document.body.appendChild(rockBtn);
+document.body.appendChild(paperBtn);
+document.body.appendChild(scissorsBtn);
+
+// displays results and scores
+const resultDiv = document.createElement('div');
+resultDiv.setAttribute('id', 'result');
+document.body.appendChild(resultDiv);
+
+const scoreDiv = document.createElement('div');
+scoreDiv.setAttribute('id', 'score');
+document.body.appendChild(scoreDiv);
+
+
+
